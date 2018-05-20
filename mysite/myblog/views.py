fc45870e3dd5148bb1d778d053cf068fcf7db6a5
chops77 <s.chao@f5.com>
2018-05-20 10:40:34 -0700

    Added some custom pages

--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.template import RequestContext, loader
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 
-# Create your views here.
+from myblog.models import Post
+
+def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
+
+    try:
+        post = published.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+
+    context = {'post': post}
+    return render(request, 'detail.html', context)
+
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    template = loader.get_template('list.html')
+    context = RequestContext(request, {'posts': posts})
+    body = template.render(context.flatten())
+    return HttpResponse(body, content_type="text/html")
+
+
